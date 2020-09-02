d322a98f1aa1c81f2ad84aba25b1680344f6be33
geeksainik <sairam9.0m@gmail.com>
2020-09-02 16:12:14 +0200

    push into only child branch example

---
+++ src/czechjavapractice/GitPushIntoOnlyChildBranchExample.java
@@ -0,0 +1,10 @@
+package czechjavapractice;
+
+public class GitPushIntoOnlyChildBranchExample {
+
+	public static void main(String[] args) {
+
+		System.out.println("GitPushIntoOnlyChildBranchExample");
+	}
+
+}
