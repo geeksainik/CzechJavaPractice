df11b5f374e88b00239226361aeb9b092d3968a7
geeksainik <sairam9.0m@gmail.com>
2020-09-02 16:06:44 +0200

    moved from master to child branch

---
+++ src/czechjavapractice/GitBranchSwitchExample.java
@@ -0,0 +1,10 @@
+package czechjavapractice;
+
+public class GitBranchSwitchExample {
+
+	public static void main(String[] args) {
+
+		System.out.println("we are on child branch moved from master to child branch");
+	}
+
+}
